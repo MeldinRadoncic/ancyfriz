23fcd938e3cbe84ba5974d35a93c9a6837513b37
Meldin Radoncic <meldinradoncic89@gmail.com>
2024-11-22 19:26:51 -0500

    global variable added to be easy for changing the size of the icons

--- src/AppData/NavbarData.js
+++ src/AppData/NavbarData.js
@@ -1,30 +1,32 @@
 import React from 'react';
 import { FaHome, FaUser, FaCut, FaPhone } from 'react-icons/fa';
 
+const iconSize = 15;
+
 const NavbarData = [
     {
 
         id: 1,
         title: 'Home',
         link: '/',
-        icon: <FaHome size={15} />   
+        icon: <FaHome size={iconSize} />   
     },
     {
         id: 2,
         title: 'About',
         link: '/about',
-        icon: <FaUser size={15} />
+        icon: <FaUser size={iconSize} />
     },
     {
         title: 'Services',
         link: '/services',
-        icon: <FaCut size={15}/> 
+        icon: <FaCut size={iconSize}/> 
     },
     {
         id: 3,
         title: 'Contact',
         link: '/contact',
-        icon: <FaPhone size={15}/>
+        icon: <FaPhone size={iconSize}/>
     },
 ]
 
