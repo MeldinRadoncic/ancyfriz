a328a9e015c73141559c53b223cffb003df02c72
Meldin Radoncic <meldinradoncic89@gmail.com>
2024-11-28 23:54:38 -0500

    Adding window effect

--- src/Components/Home/OurProducts.jsx
+++ src/Components/Home/OurProducts.jsx
@@ -7,31 +7,31 @@ import ThirdTitle from "../Titles/YhirdTitle";
 
 const OurProducts = () => {
   return (
-    <Section className="bg-primary-charcoal">
+    <Section className='bg-ourproducts relative'>
       {/* Title */}
       <SecondaryTitle
-        title="Our Products"
-        className="text-neutral-gray text-center"
+        title='Our Products'
+        className='text-neutral-gray text-center'
       />
 
       {/* Products Grid */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-8 px-4">
-        {OurProductsData.map((product, index) => (
-          <div
-            key={index}
-            className="relative bg-primary-charcoal rounded-lg shadow-xl overflow-hidden transition-transform hover:scale-105"
-          >
-            {/* Product Image */}
-            <div className="relative group"
-            animation='fade-up'
-            >
-              <img
-                src={product.img}
-                alt={product.title}
-                className="w-full h-64 object-cover"
-              />
-              {/* Hover Effect (Eye Icon) */}
-              {/* <div className="absolute inset-0 bg-black bg-opacity-40 flex items-center justify-center opacity-0 group-hover:opacity-100 transition">
+      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-8 px-4'>
+        {OurProductsData.map(
+          (product, index) => (
+            <div
+              key={index}
+              className='relative rounded-lg shadow-xl overflow-hidden transition-transform hover:scale-105'>
+              {/* Product Image */}
+              <div
+                className='relative group'
+                animation='fade-up'>
+                <img
+                  src={product.img}
+                  alt={product.title}
+                  className='w-full h-64 object-cover'
+                />
+                {/* Hover Effect (Eye Icon) */}
+                {/* <div className="absolute inset-0 bg-black bg-opacity-40 flex items-center justify-center opacity-0 group-hover:opacity-100 transition">
                 <a
                   href={product.link}
                   className="w-12 h-12 flex items-center justify-center bg-white rounded-full shadow-md"
@@ -40,16 +40,20 @@ const OurProducts = () => {
                   <HiEye className="text-primary-charcoal text-2xl" />
                 </a>
               </div> */}
+              </div>
+              {/* Product Title */}
+              <div className='mt-2 drop-shadow-md'>
+                <ThirdTitle
+                  title={product.title}
+                  className='text-neutral-gray uppercase'
+                />
+                <p className='paragraph text-neutral-gray'>
+                  {product.description}
+                </p>
+              </div>
             </div>
-            {/* Product Title */}
-            <div className="mt-2">
-              <ThirdTitle
-              title={product.title}
-               className="text-neutral-gray uppercase"/>
-               <p className="paragraph text-neutral-gray">{product.description}</p>
-            </div>
-          </div>
-        ))}
+          ),
+        )}
       </div>
     </Section>
   );
