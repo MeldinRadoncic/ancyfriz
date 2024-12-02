56d2464bd220a9e8e59ef99e4e3ff1fefb21f211
Meldin Radoncic <meldinradoncic89@gmail.com>
2024-12-02 10:33:06 -0500

    Adding className for the ImageLightbox

--- src/Components/Pages/GalleryPage/GalleryPage.jsx
+++ src/Components/Pages/GalleryPage/GalleryPage.jsx
@@ -1,28 +1,24 @@
 import React from "react";
-
-import Section from "../../Section";
 import ReusableBanner from "../../ReusableBanner";
+import Section from "../../Section";
 import GalleryPageData from "../../../AppData/GalleryPageData";
-
-
+import ImageLightbox from "../../ImageLightbox";
 
 const GalleryPage = () => {
-
-    return (
-        <>
-            <ReusableBanner
-                title='Galerija'
-                description='Pogledajte nasu galeriju'
-                
-
-                />
-
-                <Section>
-
-                </Section>
-        </>
-    )
+  return (
+    <>
+      <ReusableBanner
+        title='Galerija'
+        description='Pogledajte nasu galeriju'
+      />
+      <Section>
+        <ImageLightbox
+          images={GalleryPageData}
+          className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 rounded-lg shadow-lg'
+        />
+      </Section>
+    </>
+  );
 };
-
 
 export default GalleryPage;
