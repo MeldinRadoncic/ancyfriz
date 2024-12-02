440c99b72595b473ee252c0651545af553bb1da4
Meldin Radoncic <meldinradoncic89@gmail.com>
2024-12-02 09:42:31 -0500

    Adding Gallery link

--- src/AppData/NavbarData.js
+++ src/AppData/NavbarData.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaHome, FaUser, FaCut, FaPhone } from 'react-icons/fa';
+import { FaHome, FaUser, FaCut, FaPhone, FaPhotoVideo } from 'react-icons/fa';
 
 const iconSize = 15;
 
@@ -18,12 +18,19 @@ const NavbarData = [
         icon: <FaUser size={iconSize} />
     },
     {
+        id: 3,
         title: 'Usluge',
         link: '/usluge',
         icon: <FaCut size={iconSize}/> 
     },
+    {       
+        id: 4,
+        title: 'Galerija',
+        link: '/galerija',
+        icon: <FaPhotoVideo size={iconSize}/>
+    },
     {
-        id: 3,
+        id: 5,
         title: 'Kontakt',
         link: '/kontakt',
         icon: <FaPhone size={iconSize}/>
