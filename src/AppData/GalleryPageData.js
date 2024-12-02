c708ae0cd6a4449b381d1ee1bd1742783d941335
Meldin Radoncic <meldinradoncic89@gmail.com>
2024-12-02 09:45:02 -0500

    Adding GalleryPageData

---
+++ src/AppData/GalleryPageData.js
@@ -0,0 +1,84 @@
+const GalleryPageData = [
+  {
+    id: 1,
+    img: "https://via.placeholder.com/500x300?text=Image+1",
+  },
+  {
+    id: 2,
+    img: "https://via.placeholder.com/500x300?text=Image+2",
+  },
+  {
+    id: 3,
+    img: "https://via.placeholder.com/500x300?text=Image+3",
+  },
+  {
+    id: 4,
+    img: "https://via.placeholder.com/500x300?text=Image+4",
+  },
+  {
+    id: 5,
+    img: "https://via.placeholder.com/500x300?text=Image+5",
+  },
+  {
+    id: 6,
+    img: "https://via.placeholder.com/500x300?text=Image+6",
+  },
+  {
+    id: 7,
+    img: "https://via.placeholder.com/500x300?text=Image+7",
+  },
+  {
+    id: 8,
+    img: "https://via.placeholder.com/500x300?text=Image+8",
+  },
+  {
+    id: 9,
+    img: "https://via.placeholder.com/500x300?text=Image+9",
+  },
+  {
+    id: 10,
+    img: "https://via.placeholder.com/500x300?text=Image+10",
+  },
+  {
+    id: 11,
+    img: "https://via.placeholder.com/500x300?text=Image+11",
+  },
+  {
+    id: 12,
+    img: "https://via.placeholder.com/500x300?text=Image+12",
+  },
+  {
+    id: 13,
+    img: "https://via.placeholder.com/500x300?text=Image+13",
+  },
+  {
+    id: 14,
+    img: "https://via.placeholder.com/500x300?text=Image+14",
+  },
+  {
+    id: 15,
+    img: "https://via.placeholder.com/500x300?text=Image+15",
+  },
+  {
+    id: 16,
+    img: "https://via.placeholder.com/500x300?text=Image+16",
+  },
+  {
+    id: 17,
+    img: "https://via.placeholder.com/500x300?text=Image+17",
+  },
+  {
+    id: 18,
+    img: "https://via.placeholder.com/500x300?text=Image+18",
+  },
+  {
+    id: 19,
+    img: "https://via.placeholder.com/500x300?text=Image+19",
+  },
+  {
+    id: 20,
+    img: "https://via.placeholder.com/500x300?text=Image+20",
+  },
+];
+
+export default GalleryPageData;
