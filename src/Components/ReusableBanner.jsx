13dbfa8996f7ff094fcc0015811edc255b5364d2
Meldin Radoncic <meldinradoncic89@gmail.com>
2024-12-02 09:43:39 -0500

    Giving default css

--- src/Components/ReusableBanner.jsx
+++ src/Components/ReusableBanner.jsx
@@ -3,12 +3,13 @@ import React from "react";
 import MainTitle from "./Titles/MainTitle"; 
 
 
-const ReusableBanner = ({title, description, className=''}) => {
+const ReusableBanner = ({title, description, className='text-accent-rose-gold bg-primary-charcoal'}) => {
     return (
         <div className={`flex flex-col items-center justify-center py-12 md:py:16 lg:py-24 text-center ${className}`}>
             <MainTitle
                 title={title}
-            className='  text-accent-rose-gold'/>
+            className={className}
+/>
             <p className='paragraph text-neutral-pearl'>{description}</p>
         </div>
     );
