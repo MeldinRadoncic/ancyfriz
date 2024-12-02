01fd02ad682fd1e0a57c975cf4cc9ab510a1809e
Meldin Radoncic <meldinradoncic89@gmail.com>
2024-12-02 09:45:30 -0500

    Creating GalleryPage

---
+++ src/Components/Pages/GalleryPage/GalleryPage.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+import Section from "../../Section";
+import ReusableBanner from "../../ReusableBanner";
+import GalleryPageData from "../../../AppData/GalleryPageData";
+
+
+
+const GalleryPage = () => {
+
+    return (
+        <>
+            <ReusableBanner
+                title='Galerija'
+                description='Pogledajte nasu galeriju'
+                
+
+                />
+
+                <Section>
+
+                </Section>
+        </>
+    )
+};
+
+
+export default GalleryPage;
