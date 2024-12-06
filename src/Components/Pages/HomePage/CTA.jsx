fb366b16c7dad4edfd65c3268e5fd3a0c6aebe40
Meldin Radoncic <meldinradoncic89@gmail.com>
2024-12-05 21:28:49 -0500

    Adding routes on the buttons

--- src/Components/Pages/HomePage/CTA.jsx
+++ src/Components/Pages/HomePage/CTA.jsx
@@ -19,12 +19,16 @@ const CTA = () => {
           experience the difference!
         </p>
         <div className='grid grid-cols-2 gap-8 w-full lg:w-1/2 mx-auto'>
+        <a href="tel:+1234567899">
           <Button className='px-8 py-3 text-lg font-semibold bg-yellow-500 text-gray-800 rounded-sm shadow-lg hover:bg-yellow-600 transition duration-300'>
             Pozovite Nas
           </Button>
+          </a>
+          <a href="/kontakt">
           <Button className='px-8 py-3 text-lg font-semibold bg-yellow-500 text-gray-800 rounded-sm shadow-lg hover:bg-yellow-600 transition duration-300'>
             Pisite Nam
           </Button>
+          </a>
         </div>
       </div>
     </div>
