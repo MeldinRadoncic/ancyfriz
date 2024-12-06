79b205476cd28b4fcca282e7ceb28051f94269c3
Meldin Radoncic <meldinradoncic89@gmail.com>
2024-12-05 22:56:41 -0500

    Restyling CTA

--- src/Components/Pages/HomePage/CTA.jsx
+++ src/Components/Pages/HomePage/CTA.jsx
@@ -18,7 +18,7 @@ const CTA = () => {
           appointment today and
           experience the difference!
         </p>
-        <div className='grid grid-cols-2 gap-8 w-full lg:w-1/2 mx-auto'>
+        <div className='grid grid-cols-2 gap-8 w-full lg:w-1/4 mx-auto'>
         <a href="tel:+1234567899">
           <Button className='px-8 py-3 text-lg font-semibold bg-yellow-500 text-gray-800 rounded-sm shadow-lg hover:bg-yellow-600 transition duration-300'>
             Pozovite Nas
