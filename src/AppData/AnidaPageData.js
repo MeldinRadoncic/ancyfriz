47d7213a948584b768e69880b9e9c9cb90899b40
Meldin Radoncic <meldinradoncic89@gmail.com>
2024-12-11 21:25:38 -0500

    Removing the image and text from AnidaPageData and restyling NavItem links

--- src/AppData/AnidaPageData.js
+++ src/AppData/AnidaPageData.js
@@ -8,13 +8,13 @@ const AnidaPageData = [
       title: 'Anida Imamovic',
       description: 'Kao mala sam znala da ću se baviti ovim poslom. Jedva sam cekala da mogu da ostvarim svoj san i da imam svoj salon. Kada sam zavrsila srednju skolu upisala sam frizersku akademiju i od tada sam u ovom poslu. Volim svoj posao i trudim se da svaki dan budem bolja i da svojim klijentima pruzim naj',
     },
-    {
-      id: 2,
-      imageSrc: './images/AnidaPage/anidaimamovicsalon.webp',
-      altText: 'Vlasnica Salona Anida Imamovic',
-      title: 'Anida Imamovic',
-      description: 'Kao mala sam znala da ću se baviti ovim poslom. Jedva sam cekala da mogu da ostvarim svoj san i da imam svoj salon. Kada sam zavrsila srednju skolu upisala sam frizersku akademiju i od tada sam u ovom poslu. Volim svoj posao i trudim se da svaki dan budem bolja i da svojim klijentima pruzim naj',
-    },
+    // {
+    //   id: 2,
+    //   imageSrc: './images/AnidaPage/anidaimamovicsalon.webp',
+    //   altText: 'Vlasnica Salona Anida Imamovic',
+    //   title: 'Anida Imamovic',
+    //   description: 'Kao mala sam znala da ću se baviti ovim poslom. Jedva sam cekala da mogu da ostvarim svoj san i da imam svoj salon. Kada sam zavrsila srednju skolu upisala sam frizersku akademiju i od tada sam u ovom poslu. Volim svoj posao i trudim se da svaki dan budem bolja i da svojim klijentima pruzim naj',
+    // },
   ];
 
 
--- src/Components/Layout/Navbar/NavItem.jsx
+++ src/Components/Layout/Navbar/NavItem.jsx
@@ -6,7 +6,7 @@ function NavItem({
   to,
   label,
   icon = null,
-  textColor = "text-neutral-gray", // Default text color for the link
+  textColor = "text-accent-rose-gold", // Default text color for the link
   onClick,
 }) {
   // handleClick function calls the onClick function if it's provided
@@ -26,7 +26,7 @@ function NavItem({
           // Dynamically applies classes based on whether the link is active
           isActive
             ? `${textColor} font-bold border-b-2 border-${textColor}` // Active link styles: primary text, bold, with a bottom border`
-            : `${textColor} hover:text-neutral-gray transition` // Inactive link styles: neutral text, hover to neutral-gray, with transition effect`
+            : `${textColor} hover:text-accent-rose-gold transition` // Inactive link styles: neutral text, hover to neutral-gray, with transition effect`
       }>
       {/* Container for the icon and label */}
       <div className='flex items-center space-x-2'>
